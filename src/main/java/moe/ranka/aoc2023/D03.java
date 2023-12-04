5fd80c67949258f059ad8416424ea0131fac9eda
Rankarusu <48137583+Rankarusu@users.noreply.github.com>
2023-12-04 20:07:39 +0100

    solve day 3 part 2

--- src/main/java/moe/ranka/aoc2023/D03.java
+++ src/main/java/moe/ranka/aoc2023/D03.java
@@ -2,7 +2,11 @@ package moe.ranka.aoc2023;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class D03 extends Day {
 
@@ -27,7 +31,7 @@ public class D03 extends Day {
                     currentNumberBuffer.setLength(0);
                 }
 
-                if (j == map[0].length -1 && currentNumberBuffer.length() > 0
+                if (j == map[0].length - 1 && currentNumberBuffer.length() > 0
                         && isAdjacentToSymbol(map, currentNumberBuffer.length(), i, j)) {
                     nums.add(Integer.parseInt(currentNumberBuffer.toString()));
                 }
@@ -50,7 +54,54 @@ public class D03 extends Day {
 
     @Override
     public void part2() {
-        // TODO Auto-generated method stub
+        var lines = this.readFile("03.txt").split("\n");
+        char[][] map = Arrays.stream(lines).map(String::toCharArray).toArray(char[][]::new);
+
+        List<Integer> nums = new ArrayList<>();
+
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[0].length; j++) {
+                char symbol = map[i][j];
+                if (symbol == '*') {
+                    var set = isGear(map, i, j);
+                    if (set.size() == 2) {
+                        nums.add(set.stream().mapToInt(Integer::intValue).reduce(1, (a, b) -> a * b));
+                    }
+                }
+            }
+        }
+        System.out.println(nums.stream().mapToInt(Integer::intValue).sum());
+
+    }
+
+    private Set<Integer> isGear(char[][] map, int i, int j) {
+        Set<Integer> adjacentNumbers = new HashSet<>();
+        for (int k = Math.max(i - 1, 0); k <= Math.min(i + 1, map.length - 1); k++) {
+            for (int l = Math.max(0, j - 1); l <= Math.min(j + 1, map[0].length - 1); l++) {
+                char current = map[k][l];
+                if (Character.isDigit(current)) {
+                    adjacentNumbers.add(getNumber(map, k, l));
+                }
+            }
+        }
+        return adjacentNumbers;
+    }
+
+    private int getNumber(char[][] map, int i, int j) {
+        List<Character> list = new LinkedList<>();
+        list.add(map[i][j]);
+        int k = Math.max(j - 1, 0);
+        int l = Math.min(j + 1, map[0].length);
+        while (k >= 0 && Character.isDigit(map[i][k])) {
+            list.addFirst(map[i][k]);
+            k--;
+        }
+        while (l < map[0].length && Character.isDigit(map[i][l])) {
+            list.add(map[i][l]);
+            l++;
+        }
+
+        return Integer.parseInt(list.stream().map(String::valueOf).collect(Collectors.joining("")));
     }
 
 }
