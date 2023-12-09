6bb22f1535b64ed269dd9f2747ed2a97e6b9238c
Rankarusu <48137583+Rankarusu@users.noreply.github.com>
2023-12-09 11:42:57 +0100

    solve day 9 part 2

--- src/main/java/moe/ranka/aoc2023/D09.java
+++ src/main/java/moe/ranka/aoc2023/D09.java
@@ -29,8 +29,6 @@ public class D09 extends Day {
 
             } while (!prediction.getLast().stream().allMatch(a -> a == 0));
 
-            System.out.println(prediction);
-
             int sum = 0;
             // no need to go through the 0 only row
             for (int i = prediction.size() - 2; i >= 0; i--) {
@@ -44,8 +42,36 @@ public class D09 extends Day {
 
     @Override
     public void part2() {
-        // TODO Auto-generated method stub
+        var lines = this.readFile("09.txt").split("\n");
 
+        int result = 0;
+
+        for (String line : lines) {
+            List<Integer> nums = Arrays.stream(line.split(" ")).map(Integer::valueOf).toList();
+
+            List<List<Integer>> prediction = new ArrayList<>();
+            prediction.add(nums);
+
+            do {
+                List<Integer> lastRow = prediction.getLast();
+                List<Integer> newList = new ArrayList<>();
+
+                for (int i = 1; i < lastRow.size(); i++) {
+                    newList.add(lastRow.get(i) - lastRow.get(i - 1));
+                }
+                prediction.add(newList);
+
+            } while (!prediction.getLast().stream().allMatch(a -> a == 0));
+
+            int current = 0;
+            // no need to go through the 0 only row
+            for (int i = prediction.size() - 1; i >= 0; i--) {
+                current = prediction.get(i).getFirst() - current;
+            }
+            result += current;
+
+        }
+        System.out.println(result);
     }
 
 }
