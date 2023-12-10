dda77efead08287dca827a09228a7ca76dd94df3
Rankarusu <48137583+Rankarusu@users.noreply.github.com>
2023-12-10 17:30:43 +0100

    solve day 10 part 2

--- src/main/java/moe/ranka/aoc2023/D10.java
+++ src/main/java/moe/ranka/aoc2023/D10.java
@@ -49,6 +49,7 @@ public class D10 extends Day {
                 if (map[j][i] == 'S') {
                     startX = j;
                     startY = i;
+                    break;
                 }
             }
         }
@@ -104,7 +105,95 @@ public class D10 extends Day {
 
     @Override
     public void part2() {
-        // TODO Auto-generated method stub
+        var lines = this.readFile("10.txt").split("\n");
+        char[][] map = Arrays.stream(lines).map(String::toCharArray).toArray(char[][]::new);
+        char[][] visited = new char[map.length][map[0].length];
+
+        int startX = 0;
+        int startY = 0;
+
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[i].length; j++) {
+                if (map[i][j] == 'S') {
+                    startX = j;
+                    startY = i;
+
+                    map[i][j] = '-';
+                }
+            }
+        }
+
+        Queue<int[]> queue = new ArrayDeque<>();
+        queue.offer(new int[] { startX, startY });
+        visited[startY][startX] = '#';
+
+        while (!queue.isEmpty()) {
+            var current = queue.poll();
+            int x = current[0];
+            int y = current[1];
+            char symbol = map[y][x];
+
+            char[] cardinals = this.cardinalsMap.get(symbol);
+
+            for (char c : cardinals) {
+                int[] offset = offsetsMap.get(c);
+                int nextX = x + offset[0];
+                int nextY = y + offset[1];
+
+                if (nextY >= map.length || nextY < 0 || nextX >= map[0].length || nextX < 0) {
+                    // stay in bounds
+                    continue;
+                }
+
+                var nextSymbol = map[nextY][nextX];
+                if (!isPartOfPipe(c, nextSymbol) || visited[nextY][nextX] == '#') {
+                    continue;
+                }
+
+                queue.offer(new int[] { nextX, nextY });
+                visited[nextY][nextX] = '#';
+            }
+
+        }
+
+        int pointsInside = 0;
+
+        for (int i = 0; i < map.length; i++) {
+            System.out.println(map[i]);
+            for (int j = 0; j < map[i].length; j++) {
+                System.out.println(map[i][j]);
+                int count = 0;
+
+                if (visited[i][j] == '#') {
+                    continue;
+                }
+
+                for (int k = j + 1; k < map[i].length; k++) {
+                    char c = map[i][k];
+                    System.out.println("checking " + c);
+                    System.out.println(visited[i][k] == '#');
+                    if (visited[i][k] == '#' && (c == '|' || c == 'L' || c == 'J')) {
+                        System.out.println("found " + c + " incrementing..");
+                        count++;
+                    }
+
+                }
+                if (count != 0 && count % 2 == 1) {
+                    System.out.println("adding point, count " + count);
+                    pointsInside++;
+                    visited[i][j] = 'I';
+                }
+            }
+        }
+        System.out.println(pointsInside);
+
+        for (int i = 0; i < visited.length; i++) {
+            System.out.println();
+            for (int j = 0; j < visited[i].length; j++) {
+
+                System.out.print(visited[i][j] == '\u0000' ? 'O' : visited[i][j]);
+            }
+        }
 
     }
 
